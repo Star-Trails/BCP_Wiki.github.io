c94ada97d3d1e98aaabc4b53763d1456b8584ce6
Star-Trails <StarTrails01@outlook.com>
2020-09-09 17:31:43 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,6 @@ module.exports = {
     logo: '/BCP.jpg',
     nav: [
       { text: '首页', link: '/' },
-      { text: '关于', link: '/about/' },
       { text: '联系', link: 'https://space.bilibili.com/386183752/' },
       { text: '成员申请', link: 'https://ks.wjx.top/jq/69910494.aspx' },
       { text: '赞助', link: 'https://afdian.net/@bcpsv' },
